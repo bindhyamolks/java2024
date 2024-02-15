5f5513a351a1777eebba0a19e77ffbd36555ef9a
bindhyamolks <152183839+bindhyamolks@users.noreply.github.com>
2024-02-15 21:23:04 +0530

    secondtest
    secondone

---
+++ javatesting/src/javatesting/Printtest.java
@@ -0,0 +1,12 @@
+package javatesting;
+
+public class Printtest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welocome");
+		System.out.println("hello java");
+				
+	}
+
+}
