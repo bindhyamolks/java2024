b19b1e3b4c6e3d9591865a823d5d1f9d4a7fda0e
bindhyamolks <152183839+bindhyamolks@users.noreply.github.com>
2024-02-15 21:09:19 +0530

    Initial commit

---
+++ javanew/src/javanew/Testdatatypes.java
@@ -0,0 +1,24 @@
+package javanew;
+
+public class Testdatatypes {
+	
+
+	
+
+		public static void main(String[] args)
+		{
+		byte xyz= -128;
+		System.out.println(xyz);
+		
+		short x1= -32768;
+		System.out.println(x1);
+		
+		boolean x2= true;
+		System.out.println(x2);
+		
+		int x3= 1234567891;
+		System.out.println(x3);
+		}
+}
+		
+ 
