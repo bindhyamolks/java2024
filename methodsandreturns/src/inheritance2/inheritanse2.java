2703487e29e0519a82e8bea52b043fe4f0228030
bindhyamolks <152183839+bindhyamolks@users.noreply.github.com>
2024-02-26 20:09:32 +0530

    inheritanceexample

---
+++ methodsandreturns/src/inheritance2/inheritanse2.java
@@ -0,0 +1,13 @@
+package inheritance2;
+import methodsandreturns.inheritance;
+public class inheritanse2 extends inheritance{
+
+	public static void main(String[] args) {
+		add(10,20);
+		sub(100,50);
+		mul(20,60);
+		div(40,8);
+
+	}
+
+}
---
+++ methodsandreturns/src/methodsandreturns/inheritance.java
@@ -0,0 +1,29 @@
+package methodsandreturns;
+
+public class inheritance {
+
+	public static void add(int a,int b) 
+	{
+		System.out.println(a+b);
+		
+	  }
+	
+   public static void sub (int a,int b) 
+   {
+	   System.out.println(a-b);
+   }
+   
+   public static void mul(int a,int b) 
+   {
+   System.out.println(a*b);
+   
+   }
+   public static void div (int a,int b) 
+
+   {
+	   System.out.println(a/b);
+	   }
+   }
+   
+   
+   
